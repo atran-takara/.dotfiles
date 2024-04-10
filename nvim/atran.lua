e61fca3cb35b92b784717c801b1c7bd56852daa1
Andrew Tran <andrew_tran@takarabio.com>
2024-04-10 12:52:16 -0700

    remove roslyn lsp due to input lag.

--- nvim/atran.lua
+++ nvim/atran.lua
@@ -1,21 +1,6 @@
 local Util = require("lazyvim.util")
 
 return {
-  {
-    "jmederosalvarado/roslyn.nvim",
-    dependencies = { "hrsh7th/cmp-nvim-lsp" },
-    config = function()
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-      require("roslyn").setup({
-        capabilities = capabilities,
-        on_attach = function(client, buffer)
-          require("lazyvim.plugins.lsp.keymaps").on_attach(client, buffer)
-        end
-      })
-    end
-  },
-
   { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
 
   {
